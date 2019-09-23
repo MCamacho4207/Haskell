7a542481cd86672343f13399756574f60938fd99
Michael Camacho <48845578+MCamacho4207@users.noreply.github.com>
2019-09-23 20:34:45 +0100

    Add files via upload
    Uploaded source code of function

---
+++ Image Simplify/Image Simplify function.hs
@@ -0,0 +1,32 @@
+
+--Image Simplify function
+data Rectangle = Rectangle (Int, Int) (Int, Int) deriving (Eq, Show)
+simplifyRectangleList :: [Rectangle] -> [Rectangle]
+simplifyRectangleList rs = simplifyRectangleList' rs rs 
+
+simplifyRectangleList' :: [Rectangle] -> [Rectangle] -> [Rectangle]
+simplifyRectangleList' [] ts = [(Rectangle (777,777) (777,777))] 
+simplifyRectangleList' [r] ts = [r]
+simplifyRectangleList' (r:rs) (t:ts) | coincidesInAll t ts = simplifyRectangleList' (removeItem t rs) (removeItem t rs) 
+                                     | otherwise = simplifyRectangleList' (r:rs) (ts ++ [t])
+
+areaRectangle :: Rectangle -> Int
+areaRectangle (Rectangle (a,b) (c,d)) = (abs(a - c)) * (abs(b - c))
+
+rectangleCoincidesCompletely :: Rectangle -> Rectangle -> Bool
+rectangleCoincidesCompletely (Rectangle (s1,s2) (s3,s4)) (Rectangle (r1, r2) (r3,r4)) = (s1 >= r1) && (s1 <= r3) && (s2 >= r2) && (s2 <= r4) && (s3 <= r3) && (s3 >= r1) && (s4 <= r4) && (s4 >= r2)
+
+coincidesInAll :: Rectangle -> [Rectangle] -> Bool
+coincidesInAll r [] = False
+coincidesInAll r rs = or (map (rectangleCoincidesCompletely r) rs)
+
+rectangleIsAdjacent :: Rectangle -> Rectangle -> Bool
+rectangleIsAdjacent r1 r2 = False 
+
+moveNext :: [Rectangle] -> [Rectangle]
+moveNext (r:rs) = rs ++ [r]
+
+tangleList = [(Rectangle (0,0) (2,1)), (Rectangle (0,0) (3,1)), (Rectangle (0,0) (5,1)), (Rectangle (0,0) (4,1))]
+
+rec1 = (Rectangle (0,0) (5,1))
+rec2 = (Rectangle (0,0) (4,2))
